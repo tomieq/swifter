64cd02f7d4ae2f6732b6dc2f09dc598833e9d820
Tomasz Kucharski <tomieq@wp.pl>
2021-03-08 14:37:51 +0100

    Move all Sources to dedicated directory

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     .target(
       name: "Swifter", 
       dependencies: [], 
-      path: "XCode/Sources"
+      path: "Sources"
       ),
 
     .target(
@@ -31,7 +31,7 @@ let package = Package(
       dependencies: [
         "Swifter"
       ], 
-      path: "XCode/Tests"
+      path: "Tests"
     )
   ]
 )
