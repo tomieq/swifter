0d35cc7d53ef6e40bc7082ce0955287f85dc74d9
tomieq <tomieq@wp.pl>
2025-07-10 12:43:44 +0200

    Changed Data.random to not throwing

--- Sources/Utils/Data+random.swift
+++ Sources/Utils/Data+random.swift
@@ -5,7 +5,7 @@ extension Data {
     ///
     /// - Parameter length: Length of the data in bytes.
     /// - Returns: Generated data of the specified length.
-    public static func random(length: Int) throws -> Data {
+    public static func random(length: Int) -> Data {
         return Data((0 ..< length).map { _ in UInt8.random(in: UInt8.min ... UInt8.max) })
     }
 }
