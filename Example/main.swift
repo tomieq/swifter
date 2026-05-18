4a10984de730e2e464bb211d81ebfe1e4b7e919f
tomieq <tomieq@wp.pl>
2026-05-18 09:30:27 +0200

    Refactored example main

--- Example/main.swift
+++ Example/main.swift
@@ -1,14 +1,12 @@
 import Swifter
-import Dispatch
+import Foundation
 
 let server = HttpServer()
 
-let semaphore = DispatchSemaphore(value: 0)
 do {
     try server.start(9080, forceIPv4: true)
-    print("Server has started ( port = \(try server.port). Try to connect now...")
-    semaphore.wait()
+    print("Server has started (port = \(try server.port)). Try to connect now...")
+    RunLoop.main.run()
 } catch {
     print("Server start error: \(error)")
-    semaphore.signal()
 }
